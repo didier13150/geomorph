1cda4bbe7fab10f39d76205ca8c3f9f59d482bb4
Didier Fabert <dfabert@b2pweb.com>
2014-07-02 09:56:07 +0200

    Remove deprecated scripts (only usefull with autotools)

--- clean-for-vcs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-make clean
-make distclean
-find . -name 'Makefile' -delete
-find . -name 'Makefile.in' -delete
-rm -rf autom4te.cache m4
-rm -f aclocal.m4 compile config.* configure depcomp missing install-sh
